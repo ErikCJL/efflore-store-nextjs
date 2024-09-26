2b24b55fed7bae8c00ec214839df3295c8db7d9c
ErikCJL <erikcjl@gmail.com>
2024-09-26 19:46:11 -0300

    Efflore - correcting some minor issues for deploy

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -6,7 +6,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "@/components/ui/card";
-import Autoplay from "embla-carousel-autoplay";
+
 import Image from "next/image";
 import hero1 from "@/public/images/hero1.jpg";
 import hero2 from "@/public/images/hero2.jpg";
