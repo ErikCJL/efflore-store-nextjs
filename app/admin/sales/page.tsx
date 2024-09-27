a49bca0d2ff142e82904e6493a1616d1819932ef
ErikCJL <erikcjl@gmail.com>
2024-09-26 21:39:41 -0300

    creating form components and types and other components for admin

--- app/admin/sales/page.tsx
+++ app/admin/sales/page.tsx
@@ -2,21 +2,15 @@ import {
   Table,
   TableBody,
   TableCaption,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table';
-
-import { fetchAdminOrders } from '@/utils/actions';
-import { formatCurrency, formatDate } from '@/utils/format';
+} from "@/components/ui/table";
 
 async function SalesPage() {
-  const orders = await fetchAdminOrders();
-
   return (
     <Table>
-      <TableCaption>Total Orders : {orders.length}</TableCaption>
+      <TableCaption>Total Orders :0</TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead>Email</TableHead>
@@ -27,22 +21,7 @@ async function SalesPage() {
           <TableHead>Date</TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        {orders.map((order) => {
-          const { products, orderTotal, tax, shipping, createdAt, email } =
-            order;
-          return (
-            <TableRow key={order.id}>
-              <TableCell>{email}</TableCell>
-              <TableCell>{products}</TableCell>
-              <TableCell>{formatCurrency(orderTotal)}</TableCell>
-              <TableCell>{formatCurrency(tax)}</TableCell>
-              <TableCell>{formatCurrency(shipping)}</TableCell>
-              <TableCell>{formatDate(createdAt)}</TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
+      <TableBody></TableBody>
     </Table>
   );
 }
